730d34b5ba826de5ea1dfc288dd4c753cb433a1e
shadril238 <shadrilhassan@outlook.com>
2023-01-25 01:41:23 +0600

    familiar with .Net MVC

---
+++ FirstMVC/FirstMVC/Controllers/FirstController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
