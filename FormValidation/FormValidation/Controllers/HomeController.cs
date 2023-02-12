53e924fbf3f1221fd92df606ae74a3a19473b31d
shadril238 <shadrilhassan@outlook.com>
2023-02-12 15:59:35 +0600

    form validation

---
+++ FormValidation/FormValidation/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using FormValidation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Contact", "Home");
+            }
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FormValidation/FormValidation/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+
+namespace FormValidation.Models
+{
+    public class User
+    {
+        [Required]
+        [StringLength(40,ErrorMessage ="Custom error message")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Custom error message")]
+        public string Id { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Profession { get; set; }
+        [Required]
+        public string[] Hobbies { get; set; }
+        [Required]
+        public DateTime Dob { get; set; }
+
+        public User()
+        {
+            Dob= DateTime.Now;
+        }
+
+    }
+}
