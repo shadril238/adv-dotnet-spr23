9ea889fe9207b02d0fb66c067a08708b0bd1217b
shadril238 <shadrilhassan@outlook.com>
2023-03-23 19:49:08 +0600

    Intro API

---
+++ IntroAPI/IntroAPI/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using IntroAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntroAPI.Controllers
+{
+    public class ProductController : ApiController
+    {
+        [HttpGet]
+        [Route("api/products/all")]
+        public List<Product> AllProducts()
+        {
+            var products = new List<Product>();
+            for (int i = 1; i <= 10; i++)
+            {
+                products.Add(new Product()
+                {
+                    Id = i,
+                    Name = "P-" + i
+                });
+            }
+            return products;
+        }
+
+        [HttpGet]
+        [Route("api/products/names")]
+        public List<string> GetNames()
+        {
+            var products = new List<Product>();
+            for (int i = 1; i <= 10; i++)
+            {
+                products.Add(new Product()
+                {
+                    Id = i,
+                    Name = "P-" + i
+                });
+            }
+            var names = (from p in products
+                         select p.Name).ToList();
+            return names;
+        }
+    }
+}
---
+++ IntroAPI/IntroAPI/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntroAPI.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public List<string> GetStudents()
+        {
+            string[] names = new string[] { "Mery", "Shifat", "Gourob", "Prithy", "Rochi", "Apon" };
+            return names.ToList();
+        }
+
+        public string Post()
+        {
+            return "Post Called";
+        }
+        public string Put()
+        {
+            return "Put Called";
+        }
+    }
+}
