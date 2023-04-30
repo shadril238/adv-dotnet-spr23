89899e1485115be2343e7ef9e04ddfa272219c54
shadril238 <shadrilhassan@outlook.com>
2023-04-30 16:20:56 +0600

    File Upload

---
+++ ImageUpload/ImageUpload/Controllers/FileUploadController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageUpload.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        // GET: FileUpload
+        [HttpGet]
+        public ActionResult UploadFiles()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UploadFiles(HttpPostedFileBase file)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (file != null)
+                    {
+                        string path = Path.Combine(Server.MapPath("~/UploadedFiles/"), Path.GetFileName(file.FileName));
+                        file.SaveAs(path);
+                        ViewBag.FileStatus = "File uploaded successfully.";
+                    }
+                    
+                }
+                catch (Exception)
+                {
+
+                    ViewBag.FileStatus = "Error while file uploading.";
+                }
+
+            }
+            return View("UploadFiles");
+        }
+    }
+}
---
+++ ImageUpload/ImageUpload/Models/FileUploadModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace ImageUpload.Models
+{
+    public class FileUploadModel
+    {
+        [DataType(DataType.Upload)]
+        [Display(Name = "Upload File")]
+        [Required(ErrorMessage = "Please choose file to upload.")]
+        public string file { get; set; }
+    }
+}
